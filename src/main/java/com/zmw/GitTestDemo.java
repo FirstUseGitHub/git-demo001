a975dd31f4383ec70b5401c3c1fc69a2c6ce09a1
zmw <zmw@qq.com>
2022-01-21 16:30:55 +0800

    firstCommit

---
+++ src/main/java/com/zmw/GitTestDemo.java
@@ -0,0 +1,11 @@
+package com.zmw;
+
+/**
+ * @author 张明伟
+ * @version 1.0
+ **/
+public class GitTestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello,git1");
+    }
+}
